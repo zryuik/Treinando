7dc56f502106f3822089b9397418f068626b131e
zryuik <yuristronda123@hotmail.com>
2025-09-30 14:22:57 -0300

    ex feito

---
+++ Logica_de_python/ex4.py
@@ -0,0 +1,8 @@
+'''Escreva um programa que receba um número e informe se ele é par ou ímpar.'''
+
+numero = int(input("Digite um numero: "))
+
+if numero % 2  == 0:
+    print("O número é par")
+else:
+    print("O número é impar")
