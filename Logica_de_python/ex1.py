d640fed0e19cb25c315c1da066dd030f6a012ecf
zryuik <yuristronda123@hotmail.com>
2025-09-30 12:54:00 -0300

    exercicios simples em python apenas para logica de programação

---
+++ Logica_de_python/ex1.py
@@ -0,0 +1,7 @@
+'''Crie um programa que peça ao usuário seu nome e idade, e depois exiba a mensagem personalizada:
+“Olá, [nome], você tem [idade] anos!”'''
+
+nome = str(input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+
+print(f"Olá, {nome}, você tem {idade} anos")
