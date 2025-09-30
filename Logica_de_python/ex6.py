b4ee51aa39c17b83f08804a6133cf62877eabf2b
zryuik <yuristronda123@hotmail.com>
2025-09-30 15:05:04 -0300

    ex feito

--- Logica_de_python/ex6.py
+++ Logica_de_python/ex6.py
@@ -10,11 +10,17 @@ Idoso (65 ou mais)'''
 
 idade = int(input("Digite sua idade: "))
 
-if idade > 0 and idade < 13:
+if idade == 0:
+    print("Idade inválida")
+
+elif idade < 13:
     print("Voce é Criança")
+
 elif idade < 18:
     print("Voce é Adolescente")
+
 elif idade <65:
     print("Voce é Adulto")
+    
 else:
     print("Voce é Idoso")
