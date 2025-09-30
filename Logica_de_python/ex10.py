7e742a77a87ba056088d9d59e8bd9ac17ad3117c
zryuik <yuristronda123@hotmail.com>
2025-09-30 16:28:51 -0300

    pronto

---
+++ Logica_de_python/ex10.py
@@ -0,0 +1,18 @@
+'''Desenvolva uma calculadora simples que receba dois números e uma operação (+, -, *, /) e mostre o resultado'''
+
+numero_1 = float(input("Dgigite um número: "))
+numero_2 = float(input("Digite outro número: "))
+
+operação = str(input("Qual operação você escolhe? [+, -, x, /]: "))
+
+if operação == "+":
+    print(f"A soma entre os números é ({numero_1+numero_2})")
+
+elif operação == "-":
+    print(f"A ssubtração entre os números é ({numero_1-numero_2})")
+
+elif operação == "x":
+    print(f"A multiplicação entre os números é ({numero_1*numero_2})")
+
+elif operação == "/":
+    print(f"A divisão entre os números é ({numero_1/numero_2})")
