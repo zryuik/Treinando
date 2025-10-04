639e694e1630f417ba2296395cabee5b19eed673
zryuik <yuristronda123@hotmail.com>
2025-10-04 17:31:34 -0300

    .

---
+++ Logica_de_python/ex13.py
@@ -0,0 +1,15 @@
+'''Adicione ao dicionário precos_produtos um novo produto chamado "Monitor" com o preço de 3000. Em seguida, imprima o dicionário completo.
+
+Saída Esperada (Exemplo): {'Ipad': 1000, 'Iphone': 2000, 'Notebook': 5000, 'Monitor': 3000}'''
+
+
+precos_produtos=  {
+    "Ipad": 1000,
+    "Iphone": 2000,
+    "Notebook": 5000,
+    }
+
+
+precos_produtos["Monitor"] = 3000
+
+print(precos_produtos)
