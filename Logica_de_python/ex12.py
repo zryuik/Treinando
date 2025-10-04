5d6bb31b4db5eeae6e4fa0aa464ae462bdf3eab3
zryuik <yuristronda123@hotmail.com>
2025-10-04 17:25:58 -0300

    ex simples focado em dicionário

---
+++ Logica_de_python/ex12.py
@@ -0,0 +1,10 @@
+'''Acesse o dicionário precos_produtos e imprima o preço do produto "Ipad".'''
+
+precos_produtos = {
+    "Ipad": 1000,
+    "Iphone": 2000,
+    "Notebook": 5000,
+    }
+
+
+print(precos_produtos["Ipad"])
