d58649ded03f9aa1bee15f2d920c0fb81e2247df
Yure <113087829+zryuik@users.noreply.github.com>
2025-10-08 23:01:40 -0300

    Update ex1.py

--- Logica_de_python/ex1.py
+++ Logica_de_python/ex1.py
@@ -1,6 +1,6 @@
 '''Crie um programa que peça ao usuário seu nome e idade, e depois exiba a mensagem personalizada:
 “Olá, [nome], você tem [idade] anos!”'''
-
+###
 nome = str(input("Digite seu nome: "))
 idade = int(input("Digite sua idade: "))
 
