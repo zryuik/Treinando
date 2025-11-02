1d6af2182fba25a19e97822615705204125342fc
zryuik <yuristronda123@hotmail.com>
2025-11-02 19:55:55 -0300

    treinando

--- Logica_de_python/ex22.py
+++ Logica_de_python/ex22.py
@@ -1,1 +1,10 @@
 #Receba um número n e calcule a soma de todos os números de 1 até n.
+n = int(input("Digite um numero:\n "))
+soma = 0
+contador = 1
+
+while contador != n:
+    soma += contador
+    contador += 1
+
+print(f"A soma dos numeros de 1 ate {n} é {soma}")
