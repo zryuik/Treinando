6dd9056cb8810310edd7e8f5bf06f1fc262677b3
zryuik <yuristronda123@hotmail.com>
2025-11-05 22:40:54 -0300

    é

---
+++ Logica_de_python/ex23.py
@@ -0,0 +1,8 @@
+#peça 3 numeros e informe qual e maior entre os 3
+numeros = []
+for _ in range(0,3):
+    numero = int(input(f"Digite o {_+1}º numero: "))
+    numeros.append(numero)
+
+print(f"Os números digitados foram: {numeros}")
+print(f"O maior numero digitado foi {max(numeros)}")
