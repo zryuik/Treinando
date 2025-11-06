3aeb540a389d2c647ddc13a4ee39aa325fb173a7
zryuik <yuristronda123@hotmail.com>
2025-11-05 22:41:35 -0300

    é

--- Logica_de_python/ex23.py
+++ Logica_de_python/ex23.py
@@ -3,6 +3,6 @@ numeros = []
 for _ in range(0,3):
     numero = int(input(f"Digite o {_+1}º numero: "))
     numeros.append(numero)
-
+##
 print(f"Os números digitados foram: {numeros}")
 print(f"O maior numero digitado foi {max(numeros)}")
